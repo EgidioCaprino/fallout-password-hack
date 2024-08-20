a7e30b6b00e301acdee0d3e3ac033502cb8f9873
Egidio Caprino <egidio.caprino@ik.me>
2024-08-20 17:35:37 +0200

    fix: refresh link brings the user out from the application

--- src/components/Solver.tsx
+++ src/components/Solver.tsx
@@ -114,7 +114,7 @@ export default function Solver({ words: initialWords }: Props) {
         ))}
       </div>
       <a
-        href="/"
+        href={window.location.href}
         className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center justify-center mt-8"
       >
         <svg
