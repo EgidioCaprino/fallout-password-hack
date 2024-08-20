bb7227bca069df5e2bc7381f2ae8813519a12068
Egidio Caprino <egidio.caprino@ik.me>
2024-08-20 13:13:25 +0200

    chore: set the right base path for the production deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import { readFileSync } from "fs";
+
+const packageJSON = JSON.parse(readFileSync("package.json"));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath:
+    process.env.CI === "true" ? `/${packageJSON.name}` : undefined,
 };
 
 export default nextConfig;
